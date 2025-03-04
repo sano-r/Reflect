0eab657c40efe2d7e4b6d76cb6e48e5ab2c1c631
sano-r <r.sano0w0@gmail.com>
2025-03-04 17:59:46 +0900

    マージ修正

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
   Box,
   Heading,
@@ -7,16 +7,16 @@ import {
   VStack,
   Input,
   HStack,
-} from '@chakra-ui/react';
-import { PrimaryButton } from '@/components/atoms/PrimaryButton';
-import { DateInput } from '@/components/atoms/DateInput';
-import { GeneralSelect } from '@/components/atoms/GeneralSelect';
-import { TextInput } from '@/components/atoms/TextInput';
-import { Field } from '@/components/ui/field';
-import { SecondaryButton } from '@/components/atoms/SecondaryButton';
-import {Tag} from '@/components/ui/tag'
+} from "@chakra-ui/react";
+import { PrimaryButton } from "@/components/atoms/PrimaryButton";
+import { DateInput } from "@/components/atoms/DateInput";
+import { GeneralSelect } from "@/components/atoms/GeneralSelect";
+import { TextInput } from "@/components/atoms/TextInput";
+import { Field } from "@/components/ui/field";
+import { SecondaryButton } from "@/components/atoms/SecondaryButton";
+import { Tag } from "@/components/ui/tag";
 
-const projects= createListCollection({
+const projects = createListCollection({
   items: [
     { id: "test01", value: "○○社: △△システムバージョンアップ" },
     { id: "test02", value: "□□社: ××システムリプレース" },
@@ -25,19 +25,19 @@ const projects= createListCollection({
   ],
 });
 
-export function Home(){
-  const [date, setDate] = useState<string>('');
+export function Home() {
+  const [date, setDate] = useState<string>("");
   const [projectName, setProjectName] = useState<string[]>([]);
-  const [skills, setSkills] = useState<string[]>(["react","C#"]);
-  const [newSkill, setNewSkill] = useState<string>('');
-  const [keep, setKeep] = useState<string>('');
-  const [problem, setProblem] = useState<string>('');
-  const [tryItem, setTryItem] = useState<string>('');
+  const [skills, setSkills] = useState<string[]>(["react", "C#"]);
+  const [newSkill, setNewSkill] = useState<string>("");
+  const [keep, setKeep] = useState<string>("");
+  const [problem, setProblem] = useState<string>("");
+  const [tryItem, setTryItem] = useState<string>("");
 
   const handleAddSkill = () => {
     if (newSkill && !skills.includes(newSkill)) {
       setSkills([...skills, newSkill]);
-      setNewSkill('');
+      setNewSkill("");
     }
   };
 
@@ -51,37 +51,66 @@ export function Home(){
   };
 
   return (
-    <Box p={4} w={{md: 1000}}>
-      <Heading size="3xl" mb={4}>KPT 振り返り</Heading>
+    <Box p={4} w={{ md: 1000 }}>
+      <Heading size="3xl" mb={4}>
+        KPT 振り返り
+      </Heading>
       <Separator />
       <VStack p={4} gap={6}>
-        <DateInput label='日付' value={date} onChangeValue={setDate} />
-        <GeneralSelect 
-          label='プロジェクト' 
-          value={projectName} 
-          placeholder={'プロジェクト名を選択してください'} 
-          options={projects} 
-          onChangeValue={e => setProjectName(e.value)} 
+        <DateInput label="日付" value={date} onChangeValue={setDate} />
+        <GeneralSelect
+          label="プロジェクト"
+          value={projectName}
+          placeholder={"プロジェクト名を選択してください"}
+          options={projects}
+          onChangeValue={(e) => setProjectName(e.value)}
         />
         <Field label={"使用技術"}>
           <HStack gap={4}>
-            <Input value={newSkill} onChange={e => setNewSkill(e.target.value)} placeholder='技術名を入力'/>
+            <Input
+              value={newSkill}
+              onChange={(e) => setNewSkill(e.target.value)}
+              placeholder="技術名を入力"
+            />
             <SecondaryButton onClick={handleAddSkill}>追加</SecondaryButton>
-            <HStack gap={4} border={"solid"} p={2} color={"gray"} rounded={"lg"} w={60}>
-              {skills.map(skill => (<Tag key={skill} cursor={"pointer"} onClose={() => handleDeleteSkill(skill)} size={"lg"} colorPalette={"cyan"}>{skill}</Tag>))}
+            <HStack
+              gap={4}
+              border={"solid"}
+              p={2}
+              color={"gray"}
+              rounded={"lg"}
+              w={60}
+            >
+              {skills.map((skill) => (
+                <Tag
+                  key={skill}
+                  cursor={"pointer"}
+                  onClose={() => handleDeleteSkill(skill)}
+                  size={"lg"}
+                  colorPalette={"cyan"}
+                >
+                  {skill}
+                </Tag>
+              ))}
             </HStack>
           </HStack>
         </Field>
         <TextInput label={"Keep"} value={keep} onChangeValue={setKeep} />
-        <TextInput label={"Problem"} value={problem} onChangeValue={setProblem} />
+        <TextInput
+          label={"Problem"}
+          value={problem}
+          onChangeValue={setProblem}
+        />
         <TextInput label={"Try"} value={tryItem} onChangeValue={setTryItem} />
-        <PrimaryButton type='button' onClick={handleSubmit} w="300px" alignSelf={"end"}>
-            登録
+        <PrimaryButton
+          type="button"
+          onClick={handleSubmit}
+          w="300px"
+          alignSelf={"end"}
+        >
+          登録
         </PrimaryButton>
       </VStack>
-
-      
-
     </Box>
   );
-};
+}
